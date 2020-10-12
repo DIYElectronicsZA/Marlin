4e51d19dbeae9e2b434bf76690f7693ad4d4fc4f
RobThePyro <robthepyro@gmail.com>
2020-10-12 19:49:49 +0200

    Changed back to TMC2208 until BTT TMC5161 is fixed

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2227,13 +2227,13 @@
 #if HAS_TRINAMIC_CONFIG
 
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
-  #define INTERPOLATE        false  // Interpolate X/Y/Z_MICROSTEPS to 256
+  #define INTERPOLATE        true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       700        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
-    #define X_RSENSE          0.062
+    #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
 
@@ -2249,7 +2249,7 @@
     #define Y_CURRENT       700
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
-    #define Y_RSENSE          0.062
+    #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
   #endif
 
@@ -2265,7 +2265,7 @@
     #define Z_CURRENT       700
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
-    #define Z_RSENSE          0.062
+    #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
   #endif
 
@@ -2296,14 +2296,14 @@
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT      700
     #define E0_MICROSTEPS    16
-    #define E0_RSENSE         0.062
+    #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
   #endif
 
   #if AXIS_IS_TMC(E1)
     #define E1_CURRENT      700
     #define E1_MICROSTEPS    16
-    #define E1_RSENSE         0.062
+    #define E1_RSENSE         0.11
     #define E1_CHAIN_POS     -1
   #endif
 
@@ -2374,7 +2374,7 @@
    * The default SW SPI pins are defined the respective pins files,
    * but you can override or define them here.
    */
-  #define TMC_USE_SW_SPI
+  //#define TMC_USE_SW_SPI
   //#define TMC_SW_MOSI       -1
   //#define TMC_SW_MISO       -1
   //#define TMC_SW_SCK        -1
